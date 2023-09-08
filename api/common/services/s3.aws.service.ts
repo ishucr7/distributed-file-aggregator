39fa28a100caca7b830bc1b229c5c8566b1fdd33
Aashish Gaba <aashishgaba097@gmail.com>
2023-09-08 19:03:14 +0000

    add base s3 and file services in common

---
+++ api/common/services/s3.aws.service.ts
@@ -0,0 +1,12 @@
+import debug from 'debug';
+
+const log: debug.IDebugger = debug('app:s3-service');
+
+class S3Service {
+
+	constructor() {
+        // Set up boto3 client
+	}
+
+}
+export default new S3Service();
