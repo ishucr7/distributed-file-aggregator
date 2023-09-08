a49df9e301c22712fb4281aecf2e386cda890e45
Aashish Gaba <aashishgaba097@gmail.com>
2023-09-08 07:36:08 +0000

    add crud interface

---
+++ api/common/interfaces/crud.interface.ts
@@ -0,0 +1,6 @@
+export interface CRUD {
+	list: (limit: number, page: number) => Promise<any>;
+	create: (resource: any) => Promise<any>;
+	readById: (id: string) => Promise<any>;
+	deleteById: (id: string) => Promise<string>;
+}
