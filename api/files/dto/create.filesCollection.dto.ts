c756d38edfa1d1e5c563f4e6323a2c7f2ce7052e
Aashish Gaba <aashishgaba097@gmail.com>
2023-09-10 18:30:04 +0000

    Remove file generation api. Keep it within job creation

--- api/files/dto/create.filesCollection.dto.ts
+++
@@ -1,4 +0,0 @@
-export interface CreateFilesCollectionDto {
-    numberOfFiles: number;
-    numberOfEntries: number;
-}
--- api/files/dto/filesCollection.dto.ts
+++
@@ -1,3 +0,0 @@
-export interface FilesCollectionDto {
-    s3FilePaths: string[];
-}
