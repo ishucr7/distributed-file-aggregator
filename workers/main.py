e886a95f6985a3f92219bc71a6df7e2b3a79bb8b
Aashish Gaba <aashishgaba097@gmail.com>
2023-09-10 11:57:12 +0000

    add task executor and manager

---
+++ workers/main.py
@@ -0,0 +1,12 @@
+from celery import Celery
+
+app = Celery('tasks', broker='pyamqp://guest@localhost//')
+
+@app.task
+def add(x, y):
+    return x + y
+
+
+@app.task
+def abc(x, y):
+    return x + y
