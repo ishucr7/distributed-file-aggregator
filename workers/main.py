15a610e4dddfa187097ee098725358583e40a782
Aashish Gaba <aashishgaba097@gmail.com>
2023-09-14 06:43:19 +0000

    add dead letter queue code in celery

--- workers/main.py
+++ workers/main.py
@@ -1,5 +1,8 @@
 from celery import Celery
+from kombu import Exchange, Queue
+from celery import bootsteps
 from celery.exceptions import Reject
+
 import cProfile
 import os
 import requests
@@ -9,12 +12,61 @@ from utils.constants import RABBITMQ_PASS, RABBITMQ_USERNAME, RABBITMQ_HOST
 from utils.logger import get_logger
 from utils.backend import APIError
 
-app = Celery('main', broker=f'pyamqp://{RABBITMQ_USERNAME}:{RABBITMQ_PASS}@{RABBITMQ_HOST}')
+ENABLE_PROFILING = True
 
 logger = get_logger(__name__)
 requests_session = requests.Session()
+app = Celery('main', broker=f'pyamqp://{RABBITMQ_USERNAME}:{RABBITMQ_PASS}@{RABBITMQ_HOST}')
 
-ENABLE_PROFILING = True
+
+default_queue_name = 'dynamofl'
+default_exchange_name = 'dynamofl'
+default_routing_key = 'dynamofl'
+deadletter_suffix = 'deadletter'
+deadletter_queue_name = default_queue_name + f'.{deadletter_suffix}'
+deadletter_exchange_name = default_exchange_name + f'.{deadletter_suffix}'
+deadletter_routing_key = default_routing_key + f'.{deadletter_suffix}'
+
+class DeclareDLXnDLQ(bootsteps.StartStopStep):
+    """
+    Celery Bootstep to declare the DL exchange and queues before the worker starts
+        processing tasks
+    """
+    requires = {'celery.worker.components:Pool'}
+
+    def start(self, worker):
+        app = worker.app
+
+        # Declare DLX and DLQ
+        dlx = Exchange(deadletter_exchange_name, type='direct')
+
+        dead_letter_queue = Queue(
+            deadletter_queue_name, dlx, routing_key=deadletter_routing_key)
+
+        with worker.app.pool.acquire() as conn:
+            dead_letter_queue.bind(conn).declare()
+
+
+default_exchange = Exchange(default_exchange_name, type='direct')
+default_queue = Queue(
+    default_queue_name,
+    default_exchange,
+    routing_key=default_routing_key,
+    queue_arguments={
+        'x-dead-letter-exchange': deadletter_exchange_name,
+        'x-dead-letter-routing-key': deadletter_routing_key
+    })
+
+app.conf.task_queues = (default_queue, )
+
+# Add steps to workers that declare DLX and DLQ if they don't exist
+app.steps['worker'].add(DeclareDLXnDLQ)
+
+app.conf.task_default_queue = default_queue_name
+app.conf.task_default_exchange = default_exchange_name
+app.conf.task_default_routing_key = default_routing_key
+
+
 
 def manage_task(task, requests_session):
     task_manager = TaskManager(task)
