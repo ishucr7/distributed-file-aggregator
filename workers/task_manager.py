6b60c44d3d1cd05a9fc31fca60a00a43887027bd
Aashish Gaba <aashishgaba097@gmail.com>
2023-09-14 06:30:47 +0000

    add retrying logic in celery

--- workers/task_manager.py
+++ workers/task_manager.py
@@ -1,6 +1,13 @@
 from task_executor import TaskExecutor
 from utils.constants import FILE_EXTENSION
-from utils.backend import send_processed_task_update 
+from utils.backend import send_processed_task_update, APIError
+from utils.logger import get_logger
+
+logger = get_logger(__name__)
+
+class FileProcessingError(Exception):
+    """Custom exception for file processing errors."""
+    def __init__(self, message):
 
 class TaskManager:
     def __init__(self, task):
@@ -9,6 +16,10 @@ class TaskManager:
         self.output_file_path = f'{task["outputDir"]}/output.{FILE_EXTENSION}'
 
     def process(self, requests_session):
-        task_executor = TaskExecutor(self.task_file_paths, self.output_file_path)
-        task_executor.execute()
-        send_processed_task_update(requests_session, self.job_id, self.task_file_paths, self.output_file_path)
+        try:
+            task_executor = TaskExecutor(self.task_file_paths, self.output_file_path)
+            task_executor.execute()
+        except Exception as e:
+            logger.exception()
+            raise FileProcessingError(f'Error in processing file: should be retried; Error: {e}')
+        send_processed_task_update(requests_session, self.job_id, self.task_file_paths, self.output_file_path)            
